c2bbbce57ee226934c0a93221597ae6710f7d809
Guilherme Vieira <guilherme.paiva@avenuecode.com>
2021-09-26 22:59:05 -0300

    feat: create styled components global styles

---
+++ src/styles/global.js
@@ -0,0 +1,32 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+	* {
+		padding: 0;
+		margin: 0;
+		border: 0;
+		outline: 0;
+		font-family: "Roboto", "Helvetica", "Arial", sans-serif;
+		box-shadow: none;
+		box-sizing: border-box;
+	}
+
+	h1,
+	h2,
+	h3,
+	h4 {
+		line-height: 1.2;
+	}
+
+	html {
+		scroll-behavior: smooth;
+	}
+
+	a {
+		text-decoration: none;
+	}
+
+	button {
+		cursor: pointer;
+	}
+`;
