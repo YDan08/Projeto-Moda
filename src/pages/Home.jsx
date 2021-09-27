7e409eed5393baccec59c71cbb3098bf998da49d
YDan08 <dasyamashita@gmail.com>
2021-09-27 07:59:25 -0300

    ajustar o home e o integrantes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,8 +6,6 @@ import Roupas from "../components/Roupas";
 const Home = (props) => {
 
 	const roupas = props.roupas
-	/* 	const listaRoupas = roupas.sort(() => Math.random() - 0.5) */
-	/*const lista = listaRoupas.slice(0, 6) */
 
 	const limite = [29.9, 219.9]
 
