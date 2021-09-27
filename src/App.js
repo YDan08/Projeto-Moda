89ae105e08facb8896e5d674becb3422d44498c2
Guilherme Vieira <guilherme.paiva@avenuecode.com>
2021-09-26 22:59:17 -0300

    refactor: remove unused css import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import "./styles/app.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
